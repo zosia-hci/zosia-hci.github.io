03300bd01da12e26f985112896bc0165999aab4d
zosia <rose.lindgom@gmail.com>
2026-09-17 10:45:27 +0200

    new cv

--- js/scroller.js
+++ js/scroller.js
@@ -1,4 +1,29 @@
 $(function(){
+    // Select the node that will be observed for mutations
+    const targetNode = document.getElementById("scroller");
+
+    // Options for the observer (which mutations to observe)
+    const config = { attributes: true, childList: true, subtree: true };
+
+    // Callback function to execute when mutations are observed
+    const callback = (mutationList, observer) => {
+    for (const mutation of mutationList) {
+        if (mutation.type === "childList") {
+        console.log("A child node has been added or removed.");
+        } else if (mutation.type === "attributes") {
+        console.log(`The ${mutation.attributeName} attribute was modified.`);
+        }
+    }
+    
+    };
+
+    // Create an observer instance linked to the callback function
+    const observer = new MutationObserver(callback);
+
+    // Start observing the target node for configured mutations
+    observer.observe(targetNode, config);
+
+
     var scroller = $('#scroller div.innerScrollArea');
     var scrollerContent = scroller.children('ul');
     scrollerContent.children().clone().appendTo(scrollerContent);
@@ -7,6 +32,7 @@ $(function(){
         var $this = $(this);
         $this.css('left', curX);
         curX += $this.outerWidth(true);
+        console.log($this);
     });
     var fullW = curX / 2;
     var viewportW = scroller.width();
